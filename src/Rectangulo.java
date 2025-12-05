c665bad5ca9575bffc721a06702f02224e2d5a4b
Posorio <senjatse1@gmail.com>
2025-12-05 19:39:41 +0100

    Creada clase rectangulo y añadido constructor

---
+++ src/Rectangulo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 6DAWT
+ */
+public class Rectangulo {
+    float alto, ancho;
+    public Rectangulo (float l1, float l2) {
+        alto=l1;
+        ancho=l2;
+    }
+}
